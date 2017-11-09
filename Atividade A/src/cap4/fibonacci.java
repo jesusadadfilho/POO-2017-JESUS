70503b284b0cca6ec93bb772d1e4874dc842bbfd
jesusadadfilho <jesusadadfilho@gmail.com>
2017-11-08 23:49:43 -0300

    terminado

---
+++ Atividade A/src/cap4/fibonacci.java
@@ -0,0 +1,17 @@
+package cap4;
+
+public class fibonacci {
+	
+	int calculaFibonacci(int num)
+    {
+        if(num==1 || num==2)
+            return 1;
+        else
+            return calculaFibonacci(num-1) + calculaFibonacci(num-2);
+    }
+	
+	int calcularFibonacci2(int num) {
+		return (num==1 || num==2) ? 1 : calcularFibonacci2(num-1) + calcularFibonacci2(num-2);
+	}
+
+}
---
+++ Atividade A/src/cap4/main.java
@@ -0,0 +1,13 @@
+package cap4;
+
+public class main {
+	
+	public static void main(String args[]) {
+		fibonacci fibo = new fibonacci();
+		for (int i = 1; i <= 6; i++) {
+			int resultado = fibo.calculaFibonacci(i);
+			System.out.println(resultado);
+			}
+
+	}
+}
